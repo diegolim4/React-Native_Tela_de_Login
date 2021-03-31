f28dc75c7389d47cccdfc488d7bf89fb0b3748ae
Suelen Almeida <lima.j.diego@gmail.com>
2021-03-31 09:23:12 -0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, StatusBar, KeyboardAvoidingView, View, TouchableOpacity, TextInput, Image} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (    
+    <KeyboardAvoidingView style={myStyle.body}>
+      <StatusBar backgroundColor='#35AAFF' barStyle='light-content' />
+      <View style={myStyle.logo}>
+          <Image source={require('./assets/img/react.png')} />    
+      </View>      
+    
+      <View style={[myStyle.container]}>
+        <TextInput style={myStyle.input} placeholder="Email" autoCorrect={false} onChangeText={()=>{}} />     
+      
+        <TextInput style={myStyle.input} placeholder="Senha" secureTextEntry={true} autoCorrect={false} onChangeText={()=>{}} />
+      
+
+        <TouchableOpacity style={myStyle.btn}>  
+          <Text style={myStyle.bntTxt}>Acessar</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={myStyle.btnRegister}> 
+          <Text style={myStyle.txtRegister}>Criar Conta</Text> 
+        </TouchableOpacity>    
+      
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const myStyle = StyleSheet.create({
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  logo:{    
+    justifyContent:'center',
+    flex: 1,        
+  },
+
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    width:'90%',
+  },
+
+  input:{    
+    backgroundColor:'#fff',
+    color:'#222',
+    width:'90%',
+    fontSize:17,
+    borderRadius:7,
+    marginBottom:15,
+    padding:10,
+
+  },
+
+  btn:{
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#35AAFF',
+    padding:10,
+    borderRadius:7,
+    width:'90%',
+    padding:10,
+    height:45,
+  },
+  bntTxt:{
+    color:'#fff',
+    fontSize:15,
+  },
+
+  txtRegister:{
+    color:'#fff',
+  },
+
+  btnRegister:{
+    marginTop:10,
+    
+  }
+
 });
+
+
+
+  
